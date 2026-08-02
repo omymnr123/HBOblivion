92295ccd0eb0bcfdb481c6f440a5abbdccf0fb58
omymnr123 <omygijon20@gmail.com>
2026-08-02 10:29:45 +0200

    UpdaterFix1

--- Sources/Dependencies/Shared/version_info.h
+++ Sources/Dependencies/Shared/version_info.h
@@ -31,12 +31,12 @@ namespace client
 constexpr int major = 0;
 constexpr int minor = 2;
 constexpr int patch = 52;
-constexpr int build_number = 490;
+constexpr int build_number = 491;
 constexpr const char* stage = "alpha";
 constexpr const char* display_version = "0.2.52-alpha";
-constexpr const char* full_version = "0.2.52-alpha+build.490";
+constexpr const char* full_version = "0.2.52-alpha+build.491";
 } // namespace client
 
-constexpr const char* build_timestamp = "2026-08-02T10:14:24";
+constexpr const char* build_timestamp = "2026-08-02T10:20:54";
 
 } // namespace hb::version
